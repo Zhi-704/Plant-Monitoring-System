245899d0a4a7cd96c4994eb28e1509ed9bc415a6
Faris Abulula <abululb02@gmail.com>
2024-06-11 14:29:27 +0100

    tranformed data

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -1,9 +1,82 @@
 """Test Transform file"""
 
-from transform import get_country
+import datetime
+from transform import get_country, transform_data
+
+plant_data = [
+    {
+        "botanist": {
+            "email": "carl.linnaeus@lnhm.co.uk",
+            "name": "Carl Linnaeus",
+            "phone": "(146)994-1635x35992"
+        },
+        "images": {
+            "license": 45,
+            "license_name": "Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)",
+            "license_url": "https://creativecommons.org/licenses/by-sa/3.0/deed.en",
+            "medium_url": "https://perenual.com/storage/species_image/2773_epipremnum_aureum/medium/2560px-Epipremnum_aureum_31082012.jpg",
+            "original_url": "https://perenual.com/storage/species_image/2773_epipremnum_aureum/og/2560px-Epipremnum_aureum_31082012.jpg",
+            "regular_url": "https://perenual.com/storage/species_image/2773_epipremnum_aureum/regular/2560px-Epipremnum_aureum_31082012.jpg",
+            "small_url": "https://perenual.com/storage/species_image/2773_epipremnum_aureum/small/2560px-Epipremnum_aureum_31082012.jpg",
+            "thumbnail": "https://perenual.com/storage/species_image/2773_epipremnum_aureum/thumbnail/2560px-Epipremnum_aureum_31082012.jpg"
+        },
+        "last_watered": "Mon, 10 Jun 2024 14:03:04 GMT",
+        "name": "Epipremnum Aureum",
+        "origin_location": [
+                "-19.32556",
+                "-41.25528",
+                "Resplendor",
+                "BR",
+                "America/Sao_Paulo"
+        ],
+        "plant_id": 0,
+        "recording_taken": "2024-06-10 16:01:56",
+        "scientific_name": [
+            "Epipremnum aureum"
+        ],
+        "soil_moisture": 93.0958352536302,
+        "temperature": 13.137477117877957,
+        "response": 200
+    }]
 
 
 def test_get_country_valid() -> None:
     """Test valid country"""
-    country_code = "UK"
-    assert get_country(country_code) == "United Kingdom"
+    country_code = "BR"
+    assert get_country(country_code) == "Brazil"
+
+
+def test_tranform_data_name() -> None:
+    """Test valid name"""
+    data = transform_data(plant_data)[0]
+    assert data["first_name"], data["last_name"] == "Carl Linnaeus"
+
+
+def test_tranform_data_valid() -> None:
+    """Transform the useful data"""
+    assert transform_data(plant_data) == [{
+        'first_name': 'Carl',
+        'last_name': 'Linnaeus',
+        'email': 'carl.linnaeus@lnhm.co.uk',
+        'phone': '(146)994-1635x35992',
+        'soil_moisture': 93.0958352536302,
+        'temperature': 13.137477117877957,
+        'timestamp': datetime.datetime(2024, 6, 10, 16, 1, 56),
+        'plant_id': 0,
+        'regular_name': 'Epipremnum Aureum',
+        'scientific_name': ['Epipremnum aureum'],
+        'last_watered': datetime.datetime(2024, 6, 10, 14, 3, 4),
+        'town_name': 'Resplendor',
+        'TZ_identifier': 'America/Sao_Paulo',
+        'country_code': 'BR',
+        'country_name': 'Brazil'}]
+
+
+def test_tranform_data_invalid() -> None:
+    """Miss out any invalid data"""
+    plant_data = [{
+        "error": "plant not found",
+        "plant_id": 7,
+        "response": 400
+    }]
+    assert not transform_data(plant_data)
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,25 +1,64 @@
 """Transform file"""
+
+import pycountry
+from datetime import datetime
 from json import load
 
+DATE_FORMAT = "%a, %d %b %Y %H:%M:%S GMT"
+DATA_FILE = "plant.json"
 
-def get_data() -> None:
+
+def get_data() -> list[dict]:
     """Returns data from the herokuapp"""
-    with open("plant.json", encoding="utf-8") as f:
+    with open(DATA_FILE, encoding="utf-8") as f:
         return load(f)
 
 
 def get_country(country_code: str) -> str:
     """From the country code, get the country name"""
-    print(country_code)
+    country = pycountry.countries.get(alpha_2=country_code)
+    return country.name
 
 
-def main() -> None:
-    data = get_data()
-    for i in data:
-        if i.get("origin_location"):
-            country_code = i["origin_location"][3]
-            country = get_country(country_code)
+def get_name(name: str) -> list[str]:
+    """Splits the full name into first and last name"""
+    return name.split()
+
+
+def transform_data(plant_data: list[dict]) -> list[dict]:
+    """Selects useful data and transform to correct data type"""
+    data = []
+    for plant in plant_data[:1]:
+        if plant["response"] != 200:
+            continue
+
+        name = plant["botanist"]["name"]
+        first_name, last_name = name.split()
+
+        country_code = plant["origin_location"][3]
+        country_name = get_country(country_code)
+
+        data.append({
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": plant["botanist"]["email"],
+            "phone": plant["botanist"]["phone"],
+            "soil_moisture": float(plant["soil_moisture"]),
+            "temperature": float(plant["temperature"]),
+            "timestamp": datetime.fromisoformat(plant["recording_taken"]),
+            "plant_id": int(plant["plant_id"]),
+            "regular_name": plant["name"],
+            "scientific_name": plant.get("scientific_name"),
+            "last_watered": datetime.strptime(plant["last_watered"], DATE_FORMAT),
+            "town_name": plant["origin_location"][2],
+            "TZ_identifier": plant["origin_location"][4],
+            "country_code": country_code,
+            "country_name": country_name
+        })
+
+    return data
 
 
 if __name__ == "__main__":
-    main()
+    plant_data = get_data()
+    print(transform_data(plant_data))
