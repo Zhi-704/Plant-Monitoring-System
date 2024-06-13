186ebd1e513a5dbbe41efd4993531c9a7e6255f7
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-13 12:15:54 +0100

    complete load script

--- pipeline/load.py
+++ pipeline/load.py
@@ -2,7 +2,7 @@
 
 from os import environ as ENV
 from dotenv import load_dotenv
-from pymssql import connect, Connection, exceptions
+from pymssql import connect, Connection, exceptions  # pylint: disable=no-name-in-module
 from extract import get_all_plant_data
 from transform import transform_data
 
@@ -26,36 +26,33 @@ def get_connection() -> Connection:
             f"Error connecting to database: {e}") from e
 
 
-def remove_email_from_dict(reading_dict: dict) -> dict:
-    """Removes the email key-value pair from a dictionary,
-        returning the new dictionary."""
-    return reading_dict.pop("email")
-
-
 def dictionary_to_tuple(reading_dicts: list[dict]) -> list[tuple]:
     """Converts the transformed plant reading data from a list of dictionaries
         to a list of tuples, returning the list."""
-
     return [tuple(reading.values()) for reading in reading_dicts]
 
 
-def retrieve_botanist_ids_and_remove_botanist_emails(reading_dicts: list[dict], connection: Connection) -> list[dict]:
+def retrieve_botanist_ids_and_remove_botanist_emails(
+        reading_dicts: list[dict], connection: Connection) -> list[dict]:
     """Queries the database and retrieves the corresponding botanist ID for each reading
-        via their associated email in the reading dict, adding it to each plant reading data dictionary
-        and removing the email."""
+        via their associated email in the reading dict, adding it to each plant reading
+        data dictionary and removing the email."""
 
-    for reading in reading_dicts:
-        email = reading["email"]
-
-        with connection.cursor() as cursor:
+    with connection.cursor() as cursor:
+        for reading in reading_dicts:
+            email = reading["email"]
 
             query = "SELECT botanist_id FROM delta.botanist WHERE email = %s"
             cursor.execute(query, email)
 
             result = cursor.fetchone()
+            if result:
+                reading["botanist_id"] = result["botanist_id"]
+            else:
+                raise ValueError(f"Botanist with email {
+                                 email} not found in database.")
 
-        reading["botanist_id"] = result["botanist_id"]
-        reading = remove_email_from_dict(reading)
+            reading.pop("email")
 
     return reading_dicts
 
@@ -79,14 +76,19 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    reading_data = transform_data(get_all_plant_data())
+    try:
+        reading_data = transform_data(get_all_plant_data())
 
-    connection = get_connection()
-    reading_data = retrieve_botanist_ids_and_remove_botanist_emails(
-        reading_data, connection)
+        conn = get_connection()
 
-    reading_data = dictionary_to_tuple(reading_data)
+        reading_data = retrieve_botanist_ids_and_remove_botanist_emails(
+            reading_data, conn)
 
-    insert_readings(reading_data, connection)
+        reading_data = dictionary_to_tuple(reading_data)
 
-    connection.close()
+        insert_readings(reading_data, conn)
+
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}") from e
+    finally:
+        conn.close()
--- pipeline/test_load.py
+++ pipeline/test_load.py
@@ -1,16 +1,122 @@
-"""Contains the tests for the load scrip"""
+"""Contains the tests for the load script"""
 
-from load import dictionary_to_tuple
-import datetime
+from unittest.mock import patch, MagicMock
+from os import environ as ENV
+import pytest
+from load import get_connection, dictionary_to_tuple, \
+    retrieve_botanist_ids_and_remove_botanist_emails, insert_readings
+
+
+@patch("load.connect")
+def test_get_connection_successful(mock_connect):
+    """Tests that a connection is made successfully."""
+
+    mock_connect.return_value = MagicMock()
+
+    ENV["DB_HOST"] = "test_host"
+    ENV["DB_PORT"] = "1234"
+    ENV["DB_USER"] = "test_user"
+    ENV["DB_PASSWORD"] = "test_password"
+    ENV["DB_NAME"] = "test_db"
+
+    connection = get_connection()
+
+    assert connection == mock_connect.return_value
+
+    mock_connect.assert_called_once_with(
+        server="test_host",
+        port="1234",
+        user="test_user",
+        password="test_password",
+        database="test_db",
+        as_dict=True,
+    )
+
+
+@patch("load.connect")
+def test_get_connection_missing_key(mock_connect):
+    """Tests that a KeyError is raised if a credential is missing
+        from the .env file."""
+
+    mock_connect.return_value = MagicMock()
+
+    ENV = {
+        "DB_HOST": "test_host",
+        "DB_PORT": "1234",
+        "DB_PASSWORD": "test_password",
+        "DB_NAME": "test_db",
+    }
+
+    with patch.dict("os.environ", ENV, clear=True):
+        with pytest.raises(KeyError) as exc_info:
+            get_connection()
+
+    mock_connect.assert_not_called()
+    assert str(
+        exc_info.value) == "\"'DB_USER' missing from environment variables.\""
 
 
 def test_dictionary_to_tuple():
-    test_data = [{'soil_moisture': 90.62276785339806,
-                  'temperature': 13.60574842875693,
-                  'timestamp': datetime.datetime(2024, 6, 12, 16, 46, 56),
-                  'plant_id': 33,
-                  'last_watered': datetime.datetime(2024, 6, 12, 14, 5, 58),
-                  'botanist_id': 1}]
+    """Tests that all dictionaries are successfully turned into tuples."""
 
-    assert dictionary_to_tuple(test_data) == [
-        (90.62276785339806, 13.60574842875693, datetime.datetime(2024, 6, 12, 16, 46, 56), 33, datetime.datetime(2024, 6, 12, 14, 5, 58), 1)]
+    test_input = [{"key1": "value1", "key2": "value2"},
+                  {"key1": "value3", "key2": "value4"}]
+    expected_output = [("value1", "value2"), ("value3", "value4")]
+
+    assert dictionary_to_tuple(test_input) == expected_output
+
+
+def test_retrieve_botanist_ids_and_remove_botanist_emails():
+    """Tests that the output is as expected, and all methods are called
+        the correct number of times."""
+
+    mock_connection = MagicMock()
+    mock_cursor_instance = MagicMock()
+    mock_connection.cursor.return_value.__enter__.return_value = mock_cursor_instance
+
+    mock_cursor_instance.fetchone.side_effect = [
+        {"botanist_id": 1}, {"botanist_id": 2}]
+
+    reading_dicts = [
+        {"email": "email1@test.com", "other_key": "value1"},
+        {"email": "email2@test.com", "other_key": "value2"},
+    ]
+
+    expected_output = [
+        {"botanist_id": 1, "other_key": "value1"},
+        {"botanist_id": 2, "other_key": "value2"},
+    ]
+
+    result = retrieve_botanist_ids_and_remove_botanist_emails(
+        reading_dicts, mock_connection)
+
+    assert result == expected_output
+
+    assert mock_cursor_instance.execute.call_count == 2
+    assert mock_cursor_instance.fetchone.call_count == 2
+
+
+def test_insert_readings():
+    """Tests that the method is called correctly, and only once."""
+
+    mock_connection = MagicMock()
+    mock_cursor_instance = MagicMock()
+    mock_connection.cursor.return_value.__enter__.return_value = mock_cursor_instance
+
+    reading_tuples = [
+        (1, 2, "2021-01-01 00:00:00", 1, "2021-01-01", 1),
+        (3, 4, "2021-01-01 00:00:00", 2, "2021-01-01", 2),
+    ]
+
+    insert_readings(reading_tuples, mock_connection)
+
+    statement = """
+                    INSERT INTO delta.reading(soil_moisture, temperature, timestamp, plant_id, last_watered, botanist_id)
+                    VALUES
+                        (%s, %s, %s, %s, %s, %s)
+                    """
+
+    mock_cursor_instance.executemany.assert_called_once_with(
+        statement, reading_tuples)
+
+    mock_connection.commit.assert_called_once()
