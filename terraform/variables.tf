7e6cc634380f790c3c621240b3b90c98c6724b2a
Faris Abulula <abululb02@gmail.com>
2024-06-14 12:36:34 +0100

    added terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,31 @@ variable "AWS_REGION" {
     type = string
     default = "eu-west-2"
 }
+
+variable "BUCKET_NAME" {
+    type = string
+}
+
+variable "DB_HOST" {
+    type = string
+}
+
+variable "DB_PORT" {
+    type = string
+}
+
+variable "DB_USER" {
+    type = string
+}
+
+variable "DB_PASSWORD" {
+    type = string
+}
+
+variable "DB_NAME" {
+    type = string
+}
+
+variable "DB_SCHEMA" {
+    type = string
+}
