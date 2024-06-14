f582437329d03266b2fb42ccd12764f7199bc79d
eyuale <157725293+e-lemma@users.noreply.github.com>
2024-06-14 10:09:24 +0100

    complete container and lambda function

--- pipeline/lambda_function.py
+++ pipeline/lambda_function.py
@@ -6,8 +6,8 @@ from transform import transform_data
 from load import insert_to_database
 
 
-def lambda_handler(event, context):
-    """Runs """
+def handler(event, context):  # pylint: disable=unused-argument
+    """Executes the ETL Process."""
     raw_data = asyncio.run(get_all_plant_data())
     clean_data = transform_data(raw_data)
     insert_to_database(clean_data)
